89ee23ac8ad68c448fbcbc5ca189f23d3982fcf6
edwardfang <fangyd1997@outlook.com>
2017-11-19 09:26:54 -0800

    bind to port

--- app.js
+++ app.js
@@ -39,5 +39,6 @@ app.use(templating('views', {
 // add controller:
 app.use(controller());
 
-app.listen(3000);
-console.log('app started at port 3000...');
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`app started at port {port}...`);
